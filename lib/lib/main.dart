cb9b83e6f50ffce5052591ea49e398cc6c749dd6
PTSL-RASEL <aburaselahamed99@gmail.com>
2024-10-09 17:10:32 +0600

    add interceptor for api url,request,response

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       ChangeNotifierProvider(create: (context)=>ProductProvider()),
     ],child: MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'The Flutter Way - Foodly UI Kit',
+      title: 'Food App',
       theme: AppThemeData.lightThemeData,
 
       home: const OnboardingScreen(),
